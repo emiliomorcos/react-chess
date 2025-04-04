2f282f00366a1bfc147e799dcca7de2cdb32dea0
emiliomorcos <emiliomorcos1@gmail.com>
2025-04-04 13:37:29 -0600

    Cambio de eje exitoso

--- src/game/components/board/index.jsx
+++ src/game/components/board/index.jsx
@@ -4,33 +4,74 @@ import { defineInitialPositions } from "../../../constants";
 
 // Generamos tablero
 const Board = ({ numbers }) => {
-	const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
-	const [pieces, setPieces] = useState(defineInitialPositions(numbers));
+	const darkOnTop = numbers[0] === "8";
 
+	// Definimos letras de columnas
+	const letters = darkOnTop
+		? ["a", "b", "c", "d", "e", "f", "g", "h"]
+		: ["h", "g", "f", "e", "d", "c", "b", "a"];
+
+	const [pieces, setPieces] = useState(defineInitialPositions(numbers));
+	const [possiblePieceMovements, setPossiblePieceMovements] = useState([]);
+
+	// Definimos colores de cuadros con base en matriz y determinamos si hay una pieza en un square
+	const getSquareClassname = (x, y, hasPiece) => {
+		let classname = "square";
+		classname += (x + y) % 2 === 0 ? " square-light" : " square-dark";
+		classname += hasPiece ? " has-piece" : "";
+		const isPossibleMovement = possiblePieceMovements.some((movement) => {
+			return movement.x === x && movement.y === y;
+		});
+
+		classname += isPossibleMovement ? " possible-movement" : "";
+		return classname;
+	};
+	// Definimos función para manejar el click de una pieza
 	const handlePieceClick = (x, y, hasPiece) => {
 		if (!hasPiece) {
 			console.log("no piece here");
 			return;
 		}
 
-		const darkOnTop = numbers[0] === "8";
-
-		console.log(x, y);
+		console.log("Posición: ", x, y);
 		const piece = pieces.find((p) => {
 			return p.position.x === x && p.position.y === y;
 		});
 
-		// switch (piece.type) {
-		// 	case "pawn":
-		// 		var possibleMovements = darkOnTop ? piece.color === "dark" ? ;
-		// 		break;
-		// }
+		var possibleMovements = [];
+
+		switch (piece.type) {
+			case "pawn":
+				possibleMovements = darkOnTop
+					? piece.color === "dark"
+						? [{ x, y: y + 1 }]
+						: [{ x, y: y - 1 }]
+					: piece.color === "dark"
+					? [{ x, y: y - 1 }]
+					: [{ x, y: y + 1 }];
+
+				if (!piece.hasMoved) {
+					possibleMovements.push(
+						darkOnTop
+							? piece.color === "dark"
+								? { x, y: y + 2 }
+								: { x, y: y - 2 }
+							: piece.color === "dark"
+							? { x, y: y - 2 }
+							: { x, y: y + 2 }
+					);
+				}
+				console.log(piece, "Posibles movimientos: ", possibleMovements);
+				break;
+		}
+
+		setPossiblePieceMovements(possibleMovements);
 	};
 
 	return (
 		<div className="board">
-			{numbers.map((number, x) => {
-				return letters.map((letter, y) => {
+			{numbers.map((number, y) => {
+				return letters.map((letter, x) => {
 					// Verificar si hay una pieza en un square usando ".some"
 					const hasPiece = pieces.some((piece) => {
 						return piece.position.x === x && piece.position.y === y;
@@ -41,10 +82,10 @@ const Board = ({ numbers }) => {
 							className={getSquareClassname(x, y, hasPiece)}
 							onClick={() => handlePieceClick(x, y, hasPiece)}
 						>
-							{y === 0 && (
+							{x === 0 && (
 								<span className="num-span">{number}</span>
 							)}
-							{x === 7 && (
+							{y === 7 && (
 								<span className="letter-span">{letter}</span>
 							)}
 							{pieces.map((piece) => {
@@ -64,14 +105,6 @@ const Board = ({ numbers }) => {
 			})}
 		</div>
 	);
-};
-
-// Definimos colores de cuadros con base en matriz y determinamos si hay una pieza en un square
-const getSquareClassname = (x, y, hasPiece) => {
-	let classname = "square";
-	classname += (x + y) % 2 === 0 ? " square-light" : " square-dark";
-	classname += hasPiece ? " has-piece" : "";
-	return classname;
 };
 
 export default Board;
